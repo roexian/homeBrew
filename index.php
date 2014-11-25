9abf09e3f0cc5a191c542a5ce3e5898b20064902
roexian <bryan.wangler@gmail.com>
2014-11-25 11:05:24 -0600

    Basic programming projects
    Programming projects to get myself unto speed with coding in php.
    
    Started basic page to get things going

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Roexian's projects page</title>		
+	</head>
+	<body>
+		<div>
+			My page is dedicated to the learning and understanding of web programming and other ideas. In the future I hope let this serve as a blog and a repository of accumulated knowledge.
+		</div>
+	</body>
+</html>
