405e6aa8f1441971312c8080ee596b0c09133a7c
roexian <bryan.wangler@gmail.com>
2014-11-26 23:04:31 -0600

    Test update
    Test update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			My page is dedicated to the learning and understanding of web programming and other ideas. In the future I hope let this serve as a blog and a repository of accumulated knowledge.
             <br />
             <br />
-            <?php $test ?>
+            <?php echo $test ?>
 		</div>
 	</body>
 </html>
