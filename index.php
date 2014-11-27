c4b355b70512d8c2febbfd3a4f84def8d224b64b
roexian <bryan.wangler@gmail.com>
2014-11-26 23:02:02 -0600

    oops
    forgot file upload for construct

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include "dbo.php" ?>
 <html>
 	<head>
 		<title>Roexian's projects page</title>		
@@ -5,6 +6,9 @@
 	<body>
 		<div>
 			My page is dedicated to the learning and understanding of web programming and other ideas. In the future I hope let this serve as a blog and a repository of accumulated knowledge.
+            <br />
+            <br />
+            <?php $test ?>
 		</div>
 	</body>
 </html>
